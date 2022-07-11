a5996bb992e5dc5930907218053ca85b7cea1cad
xnvg <109017713+xnvg@users.noreply.github.com>
2022-07-11 11:01:17 +0700

    Add scripts to Rocket
    Thrusting by Space button

--- MyBoosterGame/Assets/Scripts/Movement.cs
+++ MyBoosterGame/Assets/Scripts/Movement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] float goUpSpeed = 1000f;
+
+    Rigidbody rd;
     void Start()
     {
-        
+        rd = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SpaceEventListener();
+    }
+
+    void SpaceEventListener()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rd.AddForce(Vector3.up*Time.deltaTime*goUpSpeed);
+        }
         
     }
 }
